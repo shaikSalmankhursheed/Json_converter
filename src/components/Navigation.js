d444690f2b8ca2df212f82f2f503ecc2a71a2a7c
Shaik Salman Khursheed <45688914+shaikSalmankhursheed@users.noreply.github.com>
2020-09-08 01:13:20 +0530

    Add files via upload

---
+++ src/components/Navigation.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../components/Navigation.css";
+import { Layout, Menu, Breadcrumb } from "antd";
+
+const Navigation = () => {
+  const { Header, Content, Footer } = Layout;
+  return (
+    <div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">Content</div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </div>
+  );
+};
+
+export default Navigation;
